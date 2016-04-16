33feb817b651ec6a2f0fb6722dac165091f06b4a
oliver <olivernina@gmail.com>
2016-04-15 22:11:10 -0400

    scripts

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+cd coco
+wget http://crcv.ucf.edu/people/phd_students/oliver/code/neuraltalk2/captions_train-val2014.zip
+unzip captions_train-val2014.zip
+rm captions_train-val2014.zip
+wget http://crcv.ucf.edu/people/phd_students/oliver/code/neuraltalk2/cocotalk.h5
+wget http://crcv.ucf.edu/people/phd_students/oliver/code/neuraltalk2/cocotalk.json
